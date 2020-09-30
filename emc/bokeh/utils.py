5106f387681d903c97ae282c759ed7dee376cddf
adam139 <yuejun.tang@gmail.com>
2020-09-30 14:04:46 +0800

    set bokeh tmp

--- emc/bokeh/utils.py
+++ emc/bokeh/utils.py
@@ -34,7 +34,7 @@ def load_from_file(filepath):
 
 def tmp_file_name(uid,ext="py"):
         "generate tmp file for py file and output html file"
-        dir = "/data/tmp"
+        dir = "/data/filestorage"
 #         dir = os.path.expanduser(predir)
 #         dir = "/home/plone/tmp" 
 #         temp = os.tempnam(dir)
